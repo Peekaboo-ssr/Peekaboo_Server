bc2c84c519ad0c82693eeeb62bf62c155e8519cd
purebver <wkddj1541@gmail.com>
2024-12-20 14:38:57 +0900

    fix: 플레이어 사망 시 itemdicard

--- apps/game/dedicated/src/handlers/game/player/player.handler.js
+++ apps/game/dedicated/src/handlers/game/player/player.handler.js
@@ -8,6 +8,8 @@ import {
   getUserByUserID,
 } from '../../../sessions/user.sessions.js';
 import { lifeResponse } from '../../../response/player/life.response.js';
+import { itemDiscardResponse } from '../../../response/item/item.response.js';
+import { itemDiscardNotification } from '../../../notifications/item/item.notification.js';
 
 export const playerStateChangeRequestHandler = async (
   socket,
@@ -102,27 +104,30 @@ export const playerAttackedRequestHandler = async (
       playerStateInfo,
     };
 
-    console.log('플레이어 상태 변경: ', playerStatePayload);
-    playerStateChangeNotification(server.game, playerStatePayload);
-
     // 만약 player가 죽었다면 아이템을 바닥에 뿌리고, 스테이지 종료를 검사한다.
     if (user.character.state === CHARACTER_STATE.DIED) {
       // TODO: 테스트 필요
-      // const length = user.character.inventory.slot.length;
+      const length = user.character.inventory.slot.length;
 
-      // for (let i = 0; i < length; i++) {
-      //   const itemId = user.character.inventory.removeInventorySlot(i);
-      //   if (!itemId) {
-      //     // 여기 나중에 합쳐줘도 괜찮을 것 같음.
-      //     itemDiscardResponse(socket, i + 1);
-      //     itemDiscardNotification(user.id, itemId);
-      //   }
-      // }
+      for (let i = 0; i < length; i++) {
+        const itemId = user.character.inventory.removeInventorySlot(i);
+        if (!itemId) {
+          // 여기 나중에 합쳐줘도 괜찮을 것 같음.
+          itemDiscardResponse(user.clientKey, server.game.socket, i + 1);
+          itemDiscardNotification(server.game, user.id, itemId);
+        }
+      }
+
+      console.log('플레이어 상태 변경: ', playerStatePayload);
+      playerStateChangeNotification(server.game, playerStatePayload);
 
       if (server.game.checkStageEnd()) {
         // 스테이지 종료 조건이 만족했다면, 스테이지를 종료시킨다.
         await server.game.endStage();
       }
+    } else {
+      console.log('플레이어 상태 변경: ', playerStatePayload);
+      playerStateChangeNotification(server.game, playerStatePayload);
     }
   } catch (e) {
     handleError(e);
